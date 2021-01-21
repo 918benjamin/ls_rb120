15e830b91fa760dd6f1d346af909117100c755f7
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-01-21 12:53:45 -0600

    Refactor rubocop offenses

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -18,29 +18,15 @@ class Move
   end
 
   def >(other_move)
-    if rock?
-      return true if other_move.scissors?
-      return false
-    elsif paper?
-      return true if other_move.rock?
-      return false
-    elsif scissors?
-      return true if other_move.paper?
-      return false
-    end
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
   end
 
   def <(other_move)
-    if rock?
-      return true if other_move.paper?
-      return false
-    elsif paper?
-      return true if other_move.scissors?
-      return false
-    elsif scissors?
-      return true if other_move.rock?
-      return false
-    end
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
   end
 
   def to_s
@@ -48,14 +34,12 @@ class Move
   end
 end
 
-
 class Player
   attr_accessor :move, :name
 
   def initialize
     set_name
   end
-
 end
 
 class Human < Player
@@ -109,10 +93,12 @@ class RPSGame
     puts "Thank you for playing Rock, Paper, Scissors. Goodbye!"
   end
 
-  def display_winner
+  def display_moves
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}."
+  end
 
+  def display_winner
     if human.move > computer.move
       puts "#{human.name} won!"
     elsif human.move < computer.move
@@ -132,15 +118,16 @@ class RPSGame
       puts "Sorry, must be y or n."
     end
 
-    answer == 'y' ? true : false
+    answer.downcase == 'y'
   end
 
   def play
     display_welcome_message
-    
+
     loop do
       human.choose
       computer.choose
+      display_moves
       display_winner
       break unless play_again?
     end

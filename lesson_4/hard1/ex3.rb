9176116be6975a0cb66949b71e3bb8af94dfebbf
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-01-28 08:07:09 -0600

    Add completed ex3 of hard 1

--- lesson_4/hard1/ex3.rb
+++ lesson_4/hard1/ex3.rb
@@ -6,3 +6,285 @@ number of hulls or propellers. How would you modify the vehicles code to
 incorporate a new Motorboat class?
 
 =end
+
+### Option 1 - Boat parent class that mixes in Moveable and from which Catamaran
+# and Motorboat both inherit. Use the defaults to set Motorboat hull and propeller.
+
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
+class Boat
+  include Moveable
+
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers=1, num_hulls=1, km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_capacity = liters_of_fuel_capacity
+    self.fuel_efficiency = km_traveled_per_liter
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+  end
+end
+
+# This feels weird because there really is no difference between the two without
+# some custom code within each subclass. I guess this accounts for other differences
+# not specified in the problem.
+class Catamaran < Boat
+end
+
+class Motorboat < Boat
+end
+
+### Option 2 - Boat parent class that mixes in Moveable and from which Catamaran
+# and Motorboat both inherit. Specify the Motorboat values for propeller and hull
+# in the custom initialize methods. Seems repetitive.
+
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
+class Boat
+  include Moveable
+
+  attr_reader :propeller_count, :hull_count
+end
+
+class Catamaran < Boat
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_capacity = liters_of_fuel_capacity
+    self.fuel_efficiency = km_traveled_per_liter
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+  end
+end
+
+class Motorboat < Boat
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_capacity = liters_of_fuel_capacity
+    self.fuel_efficiency = km_traveled_per_liter
+    @propeller_count = 1
+    @hull_count = 1
+  end
+end
+
+### OR
+
+class Motorboat < Boat
+  def initialize(num_propellers=1, num_hulls=1, km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_capacity = liters_of_fuel_capacity
+    self.fuel_efficiency = km_traveled_per_liter
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+  end
+end
+
+### Option 3 - Only define the propeller_count and hull count at the Catamaran
+# class level. Seems like not a great solution long term if there are other boats
+# that might need propeller count and hull count.
+
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
+class Boat
+  include Moveable
+end
+
+class Catamaran < Boat
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_capacity = liters_of_fuel_capacity
+    self.fuel_efficiency = km_traveled_per_liter
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+  end
+end
+
+class Motorboat < Boat
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_capacity = liters_of_fuel_capacity
+    self.fuel_efficiency = km_traveled_per_liter
+  end
+end
+
+### Option 4 (Best) - 
+
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
+class Boat
+  include Moveable
+
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_capacity = liters_of_fuel_capacity
+    self.fuel_efficiency = km_traveled_per_liter
+    @propeller_count = num_propellers
+    @hull_count = num_hulls
+  end
+end
+
+class Catamaran < Boat
+end
+
+class Motorboat < Boat
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end

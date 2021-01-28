65718110c9aebc45c76f4b19100f49f1c95dfdf2
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-01-27 18:01:33 -0600

    Add completed ex7 of medium 1

---
+++ lesson_4/medium1/ex7.rb
@@ -0,0 +1,20 @@
+=begin
+How could you change the method name below so that the method name is more clear
+and less repetitive?
+
+
+=end
+
+class Light
+  attr_accessor :brightness, :color
+
+  def initialize(brightness, color)
+    @brightness = brightness
+    @color = color
+  end
+
+  def status
+    "I have a brightness level of #{brightness} and a color of #{color}"
+  end
+
+end

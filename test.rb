d36dc9d50a40a7d0c78a936feadf42c527ea2261
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-11 08:54:50 -0600

    Add test file

--- test.rb
+++ test.rb
@@ -1,20 +1,81 @@
-class Human
-  attr_accessor :name
-  attr_reader :birth_year
-
-  def initialize(name, birth_year)
-    @name = name
-    @birth_year = birth_year
+module Armable
+  def attach_armor
+    # attach the armor
   end
-  
-  def age
-    Time.now.year - @birth_year
+
+  def remove_armor
+    # remove the armor
   end
 end
 
-me = Human.new("Ben", 1991)
-puts me.name
-puts me.birth_year
-puts me.age
-me.name = "Alberto"
-me.birth_year = 2008 # raises an exception, exactly what we intend because we don't want to allow anything to change the @birth_year of a Human object
+module SpellCastable
+  def cast_spell(spell)
+    # cast the spell
+  end
+end
+
+class Player
+  def initialize(name)
+    @name = name
+    @health = 100
+    roll_dice
+  end
+
+  # It wasn't specfied either way in the instructions, so I made #heal and #hurt public
+  # assuming they'd need to be accessed outside the class to function.
+  def heal(amount)
+    self.health += amount
+  end
+
+  def hurt(amount)
+    self.health += amount
+  end
+
+  def to_s
+    "Name: #{name}\n" +
+    "Class: #{self.class}\n" +
+    "Health: #{health}\n" +
+    "Strength: #{strength}\n" +
+    "Intelligence: #{intelligence}"
+  end
+
+  private
+
+  attr_accessor :health
+  attr_reader :name, :strength, :intelligence
+
+  def roll_dice
+    @strength = rand(2..12)
+    @intelligence = rand(2..12)
+  end
+end
+
+class Warrior < Player
+  include Armable
+
+  def initialize(name)
+    super
+    @strength += 2
+  end
+end
+
+class Paladin < Player
+  include Armable, SpellCastable
+end
+
+class Magician < Player
+  include SpellCastable
+
+  def initialize(name)
+    super
+    @intelligence += 2
+  end
+
+  private
+end
+
+class Bard < Magician
+  def create_potion
+    # create a potion
+  end
+end
